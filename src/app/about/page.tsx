addc37592b7cf1c02c5f7ae9988db35c9320ba6c
bu185015 <Batur.Unlu+ncrvoyix@ncrvoyix.com>
2025-10-30 23:54:21 -0400

    Add cinematic about section and disable whirlpool inside card

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-﻿// src/app/about/page.tsx
-export default function AboutPage() {
-    return (
-        <main style={{ padding: 24 }}>
-            <h1>About</h1>
-            <p>Coming soon.</p>
-        </main>
-    );
-}
